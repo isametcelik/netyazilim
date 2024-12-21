60803dd0b7087c9e1ee344c34b846c2f3c90c22b
İsmail Samet Çelik <i.samet.celik@outlook.com>
2024-12-22 01:41:46 +0300

    NetYazilim
    This website is designed using HTML, CSS, JS, PHP, MySQL

---
+++ netyazilim/index.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <title>Anasayfa | .NET Yazılım</title>
+    <link rel="stylesheet" href="Css/style.css">
+    <script src="https://kit.fontawesome.com/ad1087606b.js" crossorigin="anonymous"></script>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="owl/owl.carousel.min.css">
+<link rel="stylesheet" href="owl/owl.theme.default.min.css">
+</head>
+<body>
+
+    <section id="menu"> 
+        <div id="logo">.NET Yazılım</div>
+        <nav>
+            <a href=""><i class="fa-solid fa-house ikon"></i>Anasayfa</a>
+            <a href="#hakkimizda"><i class="fa-solid fa-info ikpn"></i>Hakkımızda</a>
+            <a href="#egitimler"><i class="fa-solid fa-graduation-cap ikon"></i>Eğitimler</a>
+            <a href="#ekip"><i class="fa-solid fa-people-group ikon"></i>Ekip</a>
+            <a href="#iletisim"><i class="fa-solid fa-message ikon"></i>İletişim</a>
+        </nav>
+    </section>
+
+    <section id="anasayfa">
+        <div id="black">
+
+        </div>
+
+        <div id="icerik">
+            <h2>.NET Yazılım</h2>
+            <hr width=300 align=left>
+            <p>Bilginin ve uygulamanın yenisini, doğrusunu, ilerisini ve geçerlisini sunmak .Net Yazılım'ın olmazsa olmaz gördüğü, sıkı sıkıya tutunduğu bir çalışma disiplinidir.</p>
+
+        </div>
+    </section>
+
+    <section id="hakkimizda">
+        <h3>Hakkımızda</h3>
+
+        <div id="container">
+            <div id="sol">
+                <h5 id="h5sol">Sağlam eğitim ve Akademi’ye özgü Eğitim Destek Programıyla beraberinde gelen iş imkanları ve ilgili sektör çevreleriyle yakın ilişkiler içinde olan .NET'liler ayrıcalıklı bir dünyanın kapılarını aralıyor.</h5>
+            </div>
+            <div id="sag">
+                <span>E</span>
+                <p id="psag">ğitmen kadrosunda ünlü televizyoncular, uzman akademisyenler, deneyimli tasarımcıların, senior yazılımcıların ve dijital dünyanın üstadlarının bir araya geldiği .NET Yazılım bu yönüyle de farklı ve yenilikçi olmayı başarmaktadır.</p>
+            </div>
+            <img src="img/linkedlnmasterclass1-1024x576.jpg" alt="" class="img-fluid mt100">
+            <p id="pson">Sürekli yenilenen ve gelişen dijital dünyanın yeni nesil uzmanlarını yetiştirmek için biz hazırız. Peki sen bu dünyanın kapılarını aralamaya hazır mısın?</p>
+        </div>
+    </section>
+
+    <section id="egitimler">
+        <div class="container">
+            <h3>Eğitimler</h3>
+            <div class="owl-carousel owl-theme">
+                <div class="card item" data-merge=1.5>
+                    <img src="img/r3.jpg" alt="" class="img-fluid">
+                    <h5 class="baslikcard">Sıfırdan Front-End Bootcamp</h5>
+                    <p class="cardp">Bu eğitim; programlamaya yeni başlamak isteyenler ve sektöre hazırlık yapmak isteyenler için tasarlanmıştır.</p>
+                </div>
+
+                <div class="card item" data-merge=1.5>
+                    <img src="img/r4.jpg" alt="" class="img-fluid">
+                    <h5 class="baslikcard">Sıfırdan Back-End Bootcamp</h5>
+                    <p class="cardp">Bu eğitim; programlamaya yeni başlamak isteyenler ve sektöre hazırlık yapmak isteyenler için tasarlanmıştır.</p>
+                </div>
+
+                <div class="card item" data-merge=1.5>
+                    <img src="img/r5.jpg" alt="" class="img-fluid">
+                    <h5 class="baslikcard">React ile Front-End Yazılım Uzmanlığı Bootcamp</h5>
+                    <p class="cardp">Bu eğitim; sektöre hazırlık yapmak isteyenler için tasarlanmıştır.</p>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
+    <section id="ekip">
+        <div class="container">
+            <h3 id="ekiph3">Ekip</h3>
+
+            <div class="sutun">
+                <img src="img/IMG_3933.jpg" alt="" class="img-fluid oval">
+                <h4 class="ekipisim">İsmail Samet Çelik</h4>
+                <p class="ekipp">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+                <div class="ekip-ikon">
+                    <a href="https://www.linkedin.com/in/ismail-samet-çelik-4099bb247/"><i class="fa-brands fa-linkedin social"></i></a>
+                    <a href="https://www.instagram.com/isametcelik/"><i class="fa-brands fa-instagram social"></i></a>
+                    <a href="#"><i class="fa-brands fa-twitter social"></i></a>
+                </div>
+            </div>
+
+
+            <div class="sutun">
+                <img src="img/ekiptaha.jpeg" alt="" class="img-fluid oval">
+                <h4 class="ekipisim">Taha Berk Erdan</h4>
+                <p class="ekipp">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+                <div class="ekip-ikon">
+                    <a href="https://www.instagram.com/tahaerdan_/"><i class="fa-brands fa-linkedin social"></i></a>
+                    <a href="https://www.instagram.com/tahaerdan_/"><i class="fa-brands fa-instagram social"></i></a>
+                    <a href="#"><i class="fa-brands fa-twitter social"></i></a>
+                </div>
+            </div>
+
+
+            <div class="sutun">
+                <img src="img/ekipmali.jpeg" alt="" class="img-fluid oval">
+                <h4 class="ekipisim">Mehmet Ali Tok</h4>
+                <p class="ekipp">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+                <div class="ekip-ikon">
+                    <a href="https://www.instagram.com/mehmettalitk/"><i class="fa-brands fa-linkedin social"></i></a>
+                    <a href="https://www.instagram.com/mehmettalitk/"><i class="fa-brands fa-instagram social"></i></a>
+                    <a href="#"><i class="fa-brands fa-twitter social"></i></a>
+                </div>
+            </div>
+
+            <div class="sutun">
+                <img src="img/zem.jpeg" alt="" class="img-fluid oval">
+                <h4 class="ekipisim">Mert Zemzemoğlu</h4>
+                <p class="ekipp">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+                <div class="ekip-ikon">
+                    <a href="https://www.linkedin.com/in/mert-zemzemoğlu-a426122a5/"><i class="fa-brands fa-linkedin social"></i></a>
+                    <a href="https://www.instagram.com/mertzmzmoglu5/"><i class="fa-brands fa-instagram social"></i></a>
+                    <a href="#"><i class="fa-brands fa-twitter social"></i></a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section id="iletisim">
+        <div class="container">
+            <h3 id="h3iletisim">İletişim</h3>
+
+            <form action="index.php" method="post">
+                
+            <div id="iletisimopak">
+                <div id="formgroup">
+                    <div id="solform">
+                        <input type="text" name="isim" placeholder="Ad Soyad" required class="form-control">
+                        <input type="text" name="tel" placeholder="Telefon Numarası" required class="form-control">
+                    </div>
+                    <div id="sagform">
+                        <input type="email" name="mail" placeholder="Email Adresiniz" required class="form-control">
+                        <input type="text" name="konu" placeholder="Konu Başlığı" required class="form-control">
+                    </div>
+                    
+                    <textarea name="mesaj" id="" cols="30" placeholder="Mesaj Giriniz" rows="10" required class="form-control"></textarea>
+                    <input type="submit"
+                    value="Gönder">
+                </div>
+
+                <div id="adres">
+
+                    <h4>Adres :</h4>
+                    <p>Fuat Morel Mah.</p>
+                    <p>Ali Kaya Mutlu Caddesi</p>
+                    <p>27152. sokak No:2</p>
+                    <p>+90 551 250 55 94</p>
+                    <p>+90 552 597 33 54</p>
+                </div>
+
+            </div>
+            </form>
+
+            <footer>
+
+                <div id="copyright">2024 | Tüm Hakları Saklıdır.</div>
+
+                <div id="socialfooter">
+                    <a href="https://www.linkedin.com/in/ismail-samet-çelik-4099bb247/"><i class="fa-brands fa-linkedin social"></i></a>
+                    <a href="https://www.instagram.com/isametcelik/"><i class="fa-brands fa-instagram social"></i></a>
+                    <a href="#"><i class="fa-brands fa-twitter social"></i></a>
+                </div>
+
+
+                <a href="#menu"><i class="fa-solid fa-arrow-up" id="up"></i></a>
+
+            </footer>
+
+        </div>
+    </section>
+
+
+    <script
+  src="https://code.jquery.com/jquery-3.7.1.slim.min.js"
+  integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8="
+  crossorigin="anonymous"></script>
+    <script src="owl/owl.carousel.min.js"></script>
+
+    <script src="owl/script.js"></script>
+</body>
+</html>
+
+<?php
+
+include("baglanti.php");
+
+if(isset($_POST["isim"], $_POST["tel"], $_POST["mail"], $_POST["konu"], $_POST["mesaj"]))
+{
+    $adsoyad=$_POST["isim"];
+    $telefon=$_POST["tel"];
+    $email=$_POST["mail"];
+    $konu= $_POST["konu"];
+    $mesaj= $_POST["mesaj"];
+
+    $ekle="INSERT INTO iletisim (adsoyad, telefon, email, konu, mesaj) VALUES ('".$adsoyad."','".$telefon."','".$email."','".$konu."','".$mesaj."')";
+
+    if($baglan->query($ekle)===TRUE)
+    {
+        echo "<script>alert('Mesajınız Başarı ile Gönderilmiştir')</script>";
+    }
+
+    else{
+        echo"<script>alert('Mesajınız Gönderilirken Bir Hata Oluştu')</script>";
+    }
+}
+
+?>
